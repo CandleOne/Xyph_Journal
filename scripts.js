6a4fa6ede48bb2e83e9792c93477ff9dde14a5d2
Reese <96293514+mdp1289@users.noreply.github.com>
2023-05-10 14:24:21 +0000

    fdgnbfg

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 const nightModeToggle = document.getElementById('night-mode-toggle');
 const fileInput = document.getElementById('file-input');
-const noteText = document.getElementById('note-text');
+const noteEditor = document.getElementById('note-editor');
 const saveTextFileButton = document.getElementById('save-text-file-button');
 const saveImageButton = document.getElementById('save-image-button');
 const textToSpeechButton = document.getElementById('text-to-speech-button');
@@ -9,108 +9,109 @@ const loadFileButton = document.getElementById('load-file-button');
 const wordCounter = document.getElementById('word-counter');
 
 nightModeToggle.addEventListener('click', () => {
-    document.body.classList.toggle('night-mode');
-  });
+  document.body.classList.toggle('night-mode');
+});
 
-  loadFileButton.addEventListener('click', () => {
-    fileInput.click();
-  });
+loadFileButton.addEventListener('click', () => {
+  fileInput.click();
+});
 
-  var quill = new Quill('#note-text', {
-    theme: 'snow'
-  });
-  
-  
+const quill = new Quill(noteEditor, {
+  modules: {
+    toolbar: '#toolbar'
+  },
+  theme: 'snow'
+});
 
-  fileInput.addEventListener('change', (event) => {
-    const file = event.target.files[0];
-    if (file) {
-      const reader = new FileReader();
-      reader.onload = (e) => {
-        noteText.value = e.target.result;
-      };
-      reader.readAsText(file);
-    }
-  });
-
-  const autosaveInterval = 30000; // 30 seconds
-const localStorageKey = 'xyph-journal-autosave';
-
-noteText.value = localStorage.getItem(localStorageKey) || '';
-
-setInterval(() => {
-    localStorage.setItem(localStorageKey, noteText.value);
-  }, autosaveInterval);
-
-  function updateWordCount() {
-    var text = quill.getText();
-    var words = text.split(/\s+/).length;
-    var characters = text.length;
-    document.getElementById('word-counter').innerHTML = 'Words: ' + words + ' | Characters: ' + characters;
-  }
-  
-
-saveTextFileButton.addEventListener('click', () => {
-  const data = noteText.value;
-  const fileName = prompt('Please enter a file name:');
-  if (fileName !== null) {
-    const a = document.createElement('a');
-    a.href = `data:text/plain;charset=utf-8,${encodeURIComponent(data)}`;
-    a.download = `${fileName}.txt`;
-    document.body.appendChild(a);
-    a.click();
-    document.body.removeChild(a);
+fileInput.addEventListener('change', (event) => {
+  const file = event.target.files[0];
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      quill.setText(e.target.result);
+    };
+    reader.readAsText(file);
   }
 });
 
+const autosaveInterval = 30000; // 30 seconds
+const localStorageKey = 'xyph-journal-autosave';
+
+quill.setText(localStorage.getItem(localStorageKey) || '');
+
+setInterval(() => {
+  localStorage.setItem(localStorageKey, noteText.value);
+}, autosaveInterval);
+
+function updateWordCount() {
+  var text = quill.getText();
+  var words = text.split(/\s+/).length;
+  var characters = text.length;
+  document.getElementById('word-counter').innerHTML = 'Words: ' + words + ' | Characters: ' + characters;
+}
+
+
+saveTextFileButton.addEventListener('click', () => {
+const data = noteText.value;
+const fileName = prompt('Please enter a file name:');
+if (fileName !== null) {
+  const a = document.createElement('a');
+  a.href = `data:text/plain;charset=utf-8,${encodeURIComponent(data)}`;
+  a.download = `${fileName}.txt`;
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+}
+});
+
 saveImageButton.addEventListener('click', () => {
-  const fileName = prompt('Please enter a file name:');
-  if (fileName !== null) {
-    const canvas = document.createElement('canvas');
-    const context = canvas.getContext('2d');
-    canvas.width = noteText.clientWidth;
-    canvas.height = noteText.clientHeight;
-    context.font = getComputedStyle(noteText).font;
-    context.fillText(noteText.value, 0, 20);
-    const dataUrl = canvas.toDataURL();
-    const a = document.createElement('a');
-    a.href = dataUrl;
-    a.download = `${fileName}.png`;
-    document.body.appendChild(a);
-    a.click();
-    document.body.removeChild(a);
-  }
+const fileName = prompt('Please enter a file name:');
+if (fileName !== null) {
+  const canvas = document.createElement('canvas');
+  const context = canvas.getContext('2d');
+  canvas.width = noteText.clientWidth;
+  canvas.height = noteText.clientHeight;
+  context.font = getComputedStyle(noteText).font;
+  context.fillText(noteText.value, 0, 20);
+  const dataUrl = canvas.toDataURL();
+  const a = document.createElement('a');
+  a.href = dataUrl;
+  a.download = `${fileName}.png`;
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+}
 });
 
 
 textToSpeechButton.addEventListener('click', () => {
-  const utterance = new SpeechSynthesisUtterance(noteText.value);
-  speechSynthesis.speak(utterance);
+const utterance = new SpeechSynthesisUtterance(noteText.value);
+speechSynthesis.speak(utterance);
 });
 
 savePdfButton.addEventListener('click', () => {
-  const fileName = prompt('Please enter a file name:');
-  if (fileName !== null) {
-    const pdf = new jsPDF();
-    pdf.text(noteText.value, 10, 10);
-    pdf.save(`${fileName}.pdf`);
-  }
+const fileName = prompt('Please enter a file name:');
+if (fileName !== null) {
+  const pdf = new jsPDF();
+  pdf.text(noteText.value, 10, 10);
+  pdf.save(`${fileName}.pdf`);
+}
 });
 ``
 quill.on('text-change', function(delta, oldDelta, source) {
-  updateWordCount();
-  var text = quill.getText();
-  if (text.length > 0) {
-    document.getElementById('save-text-file-button').disabled = false;
-    document.getElementById('save-image-button').disabled = false;
-    document.getElementById('text-to-speech-button').disabled = false;
-    document.getElementById('save-pdf-button').disabled = false;
-  } else {
-    document.getElementById('save-text-file-button').disabled = true;
-    document.getElementById('save-image-button').disabled = true;
-    document.getElementById('text-to-speech-button').disabled = true;
-    document.getElementById('save-pdf-button').disabled = true;
-  }
+updateWordCount();
+var text = quill.getText();
+if (text.length > 0) {
+  document.getElementById('save-text-file-button').disabled = false;
+  document.getElementById('save-image-button').disabled = false;
+  document.getElementById('text-to-speech-button').disabled = false;
+  document.getElementById('save-pdf-button').disabled = false;
+} else {
+  document.getElementById('save-text-file-button').disabled = true;
+  document.getElementById('save-image-button').disabled = true;
+  document.getElementById('text-to-speech-button').disabled = true;
+  document.getElementById('save-pdf-button').disabled = true;
+}
 });
 
 
