daa185b6b4e629c8fd640c2e43aee3c175b69e1d
Reese <96293514+mdp1289@users.noreply.github.com>
2023-05-15 13:05:18 +0000

    bdtfyhtdtf

--- speech to text/script.js
+++ speech to text/script.js
@@ -1,19 +1,28 @@
-// Check if the browser supports speech recognition
-if ('SpeechRecognition' in window || 'webkitSpeechRecognition' in window) {
-    // Create a new SpeechRecognition object
-    const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
+const startButton = document.getElementById("startButton");
+const voiceInput = document.getElementById("voiceInput");
+const response = document.getElementById("response");
 
-    // Set the language for speech recognition
-    recognition.lang = 'en-US';
+startButton.addEventListener("click", () => {
+    if ('webkitSpeechRecognition' in window) {
+        const recognition = new webkitSpeechRecognition();
+        recognition.lang = 'en-US';
+        recognition.interimResults = false;
 
-    // Define the event handler for the 'result' event
-    recognition.onresult = (event) => {
-        const result = event.results[0][0].transcript;
-        document.getElementById('output').textContent = result;
-    };
+        recognition.onresult = (event) => {
+            const last = event.results.length - 1;
+            const text = event.results[last][0].transcript;
+            voiceInput.value = text;
+            respondToQuery(text);
+        };
 
-    // Start listening for voice input
-    recognition.start();
-} else {
-    document.getElementById('output').textContent = 'Speech recognition not supported in this browser.';
+        recognition.start();
+    } else {
+        alert('Speech recognition is not supported in your browser.');
+    }
+});
+
+function respondToQuery(query) {
+    // Add your logic to generate a response based on the user's input.
+    // For now, we'll just echo back the input.
+    response.value = `You said: ${query}`;
 }
