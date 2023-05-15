7f2a0da43f14593dcfb55fd84dc890abb23012c2
Reese <96293514+mdp1289@users.noreply.github.com>
2023-05-15 13:08:51 +0000

    ertb

--- speech to text/script.js
+++ speech to text/script.js
@@ -8,11 +8,23 @@ startButton.addEventListener("click", () => {
         recognition.lang = 'en-US';
         recognition.interimResults = false;
 
+        recognition.onstart = () => {
+            startButton.innerText = "Listening...";
+            startButton.disabled = true;
+            startButton.style.backgroundColor = "#ccc";
+        };
+
         recognition.onresult = (event) => {
             const last = event.results.length - 1;
             const text = event.results[last][0].transcript;
             voiceInput.value = text;
             respondToQuery(text);
+        };
+
+        recognition.onend = () => {
+            startButton.innerText = "Start Listening";
+            startButton.disabled = false;
+            startButton.style.backgroundColor = "";
         };
 
         recognition.start();
